8f866eb81dc46b24c4be11677ac0e0c582a26602
hyooosong <gythd1998@gmail.com>
2024-01-28 16:42:58 +0900

    [feature] 그룹 추가하기 - 그룹 아이콘 선택 모달

--- app/src/main/java/com/linkzip/linkzip/presentation/component/BottomDialogComponent.kt
+++ app/src/main/java/com/linkzip/linkzip/presentation/component/BottomDialogComponent.kt
@@ -1,7 +1,5 @@
 package com.linkzip.linkzip.presentation.component
-import android.util.Log
 import android.view.Gravity
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -9,23 +7,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,12 +23,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -57,6 +44,7 @@ fun BottomDialogComponent(
     onDismissRequest: () -> Unit,
     visible: Boolean,
     height : Dp,
+    horizontalMargin: Dp,
     content: @Composable () -> Unit
 ) {
     if (visible) {
@@ -65,6 +53,7 @@ fun BottomDialogComponent(
             properties = DialogProperties(
                 dismissOnBackPress = true,
                 dismissOnClickOutside = true,
+                usePlatformDefaultWidth = false,
                 securePolicy = SecureFlagPolicy.Inherit
             )
         ) {
@@ -75,6 +64,7 @@ fun BottomDialogComponent(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(height)
+                    .padding(horizontal = horizontalMargin)
                     .offset(y = (-30).dp),
                 shape = RoundedCornerShape(16.dp),
             ) {
@@ -82,11 +72,15 @@ fun BottomDialogComponent(
                     modifier = Modifier
                         .fillMaxWidth()
                         .fillMaxHeight()
+                        .background(color = LinkZipTheme.color.white)
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.bottom_dialog_top),
-                        contentDescription = "bottom_dialog_top")
+                        contentDescription = "bottom_dialog_top",
+                        modifier = Modifier.padding(top = 8.dp).fillMaxWidth(),
+                        alignment = Alignment.Center)
                     Box(modifier = Modifier
+                        .background(color = LinkZipTheme.color.white)
                         .padding(horizontal = 20.dp)
                         .padding(vertical = 30.dp)
                         .fillMaxWidth()
